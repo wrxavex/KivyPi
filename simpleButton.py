ef1545e7992e432125c7a50252e5ef9c9c13775a
wrxavex <wrxavex@gmail.com>
2016-03-22 00:41:31 +0800

    add new sample

--- simpleButton.py
+++ simpleButton.py
@@ -1,22 +1,37 @@
-import kivy
-kivy.require('1.9.1')
-
 from kivy.uix.scatter import Scatter
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 
+from kivy.clock import Clock
+
+import time
+import os
+
+os.environ['TZ'] = 'Asia/Taipei'
+time.tzset()
+
+mytext = "My Text"
+time_now = "time"
+
+
+
+
 
 class TestAPP(App):
     def build(self):
+        time_now = time.strftime('%X')
         f = FloatLayout()
         s = Scatter()
-        l = Label(text="Button!!!Drag",
+        l = Label(text=time_now,
                   font_size=150)
 
         f.add_widget(s)
         s.add_widget(l)
         return f
 
+
+
 if __name__ == "__main__":
+    Clock.schedule_interval(TestAPP, 1/60.)
     TestAPP().run()
