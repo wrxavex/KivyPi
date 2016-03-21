ce4176e6aa7184f4890f10042d84f0b95f241121
wrxavex <wrxavex@gmail.com>
2016-03-21 21:37:57 +0800

    fix syntax 3

--- kv_builder.py
+++ kv_builder.py
@@ -5,22 +5,24 @@ from kivy.lang import Builder
 root = Builder.load_string('''
 FloatLayout:
     canvas.before:
-        color:
+        Color:
             rgba: 0, 1, 0, 1
         Rectangle:
+            # self here refers to the widget i.e FloatLayout
             pos: self.pos
             size: self.size
-        Button:
-            text: 'Hello!'
-            size_hint: .5, .5
-            pos_hint: {'center_x':.5, 'center_y':.5}
-        ''')
+    Button:
+        text: 'Hello World!!'
+        size_hint: .5, .5
+        pos_hint: {'center_x':.5, 'center_y': .5}
+''')
 
 
 class MainApp(App):
 
     def build(self):
         return root
+
 
 if __name__ == '__main__':
     MainApp().run()
