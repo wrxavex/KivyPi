9372ee3c31fff31e357d3a21e13ac1ed4198b4ca
wrxavex <wrxavex@gmail.com>
2016-06-14 21:04:34 +0800

    add kivy-osc

--- kivyosc/main.py
+++ kivyosc/main.py
@@ -34,6 +34,8 @@ class Controller(FloatLayout):
         osc.sendMsg('/print/pd', dataArray=['sen2d', my_info.count], ipAddr='192.168.1.193', port=serviceport)
         osc.sendMsg('/print/pd', dataArray=['sen2d', my_info.count], ipAddr='192.168.1.160', port=serviceport)
         osc.sendMsg('/print/pd', dataArray=['sen2d', my_info.count], ipAddr='192.168.1.194', port=serviceport)
+        osc.sendMsg('/print/pd', dataArray=['sen2d', my_info.count], ipAddr='192.168.1.177', port=serviceport)
+        osc.sendMsg('/print/pd', dataArray=['sen2d', my_info.count], ipAddr='192.168.1.198', port=serviceport)
 
         print('osc send')
 
