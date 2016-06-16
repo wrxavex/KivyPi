e66dde9a6b3aea5f3d3d28316a461d19ef2906ff
wrxavex <wrxavex@gmail.com>
2016-06-16 19:35:17 +0800

    add set_id

--- kivyosc/derrick.py
+++ kivyosc/derrick.py
@@ -308,7 +308,7 @@ if __name__ == '__main__':
     osc.bind(oscid, derrick_osc, '/derrick/osc')
 
     Clock.schedule_interval(lambda *x: osc.readQueue(oscid), 0)
-
+    print (my_id.id_to_set)
     print (croparea_setter(my_id.id_to_set))
 
     # playone(my_video)
