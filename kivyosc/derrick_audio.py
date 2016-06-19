9bfacc77f96cd11bb8502fc5a3c87f1d021acb35
wrxavex <wrxavex@gmail.com>
2016-06-20 01:55:23 +0800

    method 2

--- kivyosc/derrick_audio.py
+++ kivyosc/derrick_audio.py
@@ -102,7 +102,5 @@ if __name__ == '__main__':
 
     Clock.schedule_interval(lambda *x: osc.readQueue(oscid), 0)
     print (my_id.id_to_set, 'id_to_set')
-
     play_random_audio()
-
     DerrickApp().run()
