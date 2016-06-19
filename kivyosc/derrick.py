ceef4aa3349775143c2cb8bd66f053481496edc1
wrxavex <wrxavex@gmail.com>
2016-06-19 21:00:36 +0800

    add audio

--- kivyosc/derrick.py
+++ kivyosc/derrick.py
@@ -306,7 +306,7 @@ def derrick_osc(message, *args):
         stopall()
         sleep(random.uniform(0,1))
         print ('id 4 video play before')
-        thread.start_new_thread(playone, (f4, ))
+        # thread.start_new_thread(playone, (f4, ))
         print ('id 4 after video')
         thread.start_new_thread(play_audio, (dt, ))
 
